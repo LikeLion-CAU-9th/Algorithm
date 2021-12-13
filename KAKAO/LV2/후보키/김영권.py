36962bef87a4b4ed211fbe6d86fe5d3503fbff02
youngkwon02 <youngkwon02@naver.com>
2021-12-13 16:18:34 +0900

    chore: remove 42890-후보키 test-case printing

--- KAKAO/LV2/후보키/김영권.py
+++ KAKAO/LV2/후보키/김영권.py
@@ -36,6 +36,3 @@ def is_min(key_lst, com_set):
             return False
     return True
 
-
-print(solution([["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"], [
-    "400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]))
